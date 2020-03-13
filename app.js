0e0ab6a1453897b63f42831e0397b9949429b37c
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-13 19:15:50 -0400

    Delete & filter tasks

--- app.js
+++ app.js
@@ -12,20 +12,26 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
 function addTask(e) {
   if (taskInput.value === '') {
-    alert('Add a task')
+    alert('Add a task');
   }
 
   // Create li element
   const li = document.createElement('li');
   // Add class
-  li.className = 'collection-item'
+  li.className = 'collection-item';
   // Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value))
+  li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
   const link = document.createElement('a');
   // Add class
@@ -37,9 +43,41 @@ function addTask(e) {
 
   // Append li to ul
   taskList.appendChild(li);
-  
+
   // Clear input
   taskInput.value = '';
 
-  e.preventDefault()
+  e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure you want to remove this task?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // More efficient way of clearing tasks
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
